8c04034e92f3369785e1bfb1aad1cda763207f30
Omoefe Bazunu <raniem57@gmail.com>
2023-12-21 03:11:00 +0100

    COMPLETED THE INPUT CODES

--- back.js
+++ back.js
@@ -1,6 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
     const frontBtn = document.getElementById('frontBtn');
     const backBtn = document.getElementById('backBtn');
+    
+    const companyLogo = document.querySelector('#logo');
+    companyLogo.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const cLogo = event.target.result;
+                localStorage.setItem('cLogo', cLogo);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    const staffPassport = document.querySelector('#passport');
+    staffPassport.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const sPassport = event.target.result;
+                localStorage.setItem('sPassport', sPassport);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    const staffSignature = document.querySelector('#staff-signature');
+    staffSignature.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const sSignature = event.target.result;
+                localStorage.setItem('sSignature', sSignature);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    const companySignature = document.querySelector('#company-signature');
+    companySignature.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const companySig = event.target.result;
+                localStorage.setItem('companySig', companySig);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
 
 });
 
@@ -16,19 +68,16 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
     const saveInput = () => {
-    const companyLogo = document.querySelector('#logo').value;
+
     const companyName = document.querySelector('#companyName').value;
-    const staffPassport = document.querySelector('#passport').value;
     const staffName = document.querySelector('#staffName').value;
     const staffPosition = document.querySelector('#staff-position').value;
     const countryCode = document.querySelector('#country-code').value;
     const staffPhoneNumber = document.querySelector('#staffNumber').value;
     const staffContact = countryCode + staffPhoneNumber;
     const staffEmail = document.querySelector('#staff-email').value;
-    const staffSignature = document.querySelector('#staff-signature').value;
-
-
-    //Code to retrieve the data input for the back page
+    
+     //Code to retrieve the data input for the back page
     const certificationText = document.querySelector('#certificationText').value;
     const backCompanyName = document.querySelector('#company-name').value;
     const companyAddress = document.querySelector('#company-address').value;
@@ -36,19 +85,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const companyPhoneNumber = document.querySelector('#companyPhoneNumber').value;
     const companyContact = backcountryCode + companyPhoneNumber;
     const companyEmail = document.querySelector('#company-email').value;
-    const companySignature = document.querySelector('#company-signature').value;
+    
+
+    // if(!companyName) {
+    //     window.location.href = 'editpage.html';
+    //     alert("Please input data before clicking button");
+
+    // } else {
 
     //code to store the front page input for transfer to front page
-    localStorage.setItem('cLogo', companyLogo);
+    
     localStorage.setItem('cName', companyName);
-    localStorage.setItem('sPassport', staffPassport);
     localStorage.setItem('sName', staffName);
     localStorage.setItem('sPosition', staffPosition);
     localStorage.setItem('sContact', staffContact);
     localStorage.setItem('sEmail', staffEmail);
-    localStorage.setItem('sSignature', staffSignature);
-
-
 
     //code to store the front page input for transfer to front page
     localStorage.setItem('cText', certificationText);
@@ -56,35 +107,8 @@ document.addEventListener('DOMContentLoaded', function() {
     localStorage.setItem('cAddress', companyAddress);
     localStorage.setItem('cContact', companyContact);
     localStorage.setItem('cEmail', companyEmail);
-    localStorage.setItem('cSignature', companySignature);
 
     window.location.href = 'index.html';
-};
+// };
+    };
 
-
-
-
-// backBtn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     const certificationText = document.querySelector('#certificationText').value;
-//     const backCompanyName = document.querySelector('#company-name').value;
-//     const companyAddress = document.querySelector('#company-address').value;
-//     const backcountryCode = document.querySelector('#b-country-code').value;
-//     const companyPhoneNumber = document.querySelector('#companyPhoneNumber').value;
-//     const companyContact = backcountryCode + companyPhoneNumber;
-//     const companyEmail = document.querySelector('#company-email').value;
-//     const companySignature = document.querySelector('#company-signature').value;
-
-
-//     //code to store the front page input for transfer to front page
-//     localStorage.setItem('cText', certificationText);
-//     localStorage.setItem('bcName', backCompanyName);
-//     localStorage.setItem('cAddress', companyAddress);
-//     localStorage.setItem('cContact', companyContact);
-//     localStorage.setItem('cEmail', companyEmail);
-//     localStorage.setItem('cSignature', companySignature);
-
-//     window.location.href = 'index.html';
-//     // localStorage.setItem('cName', companyName);
-//     // window.location.href = 'index.html';
-// });
--- front.js
+++ front.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+    //code to retrieve the data from the localstorage
     const companyL = localStorage.getItem('cLogo');
     const companyN = localStorage.getItem('cName');
     const staffP = localStorage.getItem('sPassport');
@@ -8,17 +9,80 @@ document.addEventListener('DOMContentLoaded', function() {
     const staffContact = localStorage.getItem('sContact');
     const staffEmail = localStorage.getItem('sEmail');
     const staffSig = localStorage.getItem('sSignature');
-
     const certText = localStorage.getItem('cText');
     const bcompanyName = localStorage.getItem('bcName');
     const companyA = localStorage.getItem('cAddress');
     const companyC = localStorage.getItem('cContact');
     const companyE = localStorage.getItem('cEmail');
-    const companySig = localStorage.getItem('cSignature');
+    const companySig = localStorage.getItem('companySig');
 
-    alert(companyL + " " + companyN + " " + staffP 
-    + " " + staffName + " " + staffPosition + " " + staffContact + " " + staffEmail + " " + 
-    staffSig + " " + certText  + " " + bcompanyName + " " + companyA 
-    + " " + companyC + " " + companyE + " " + companySig);
+    //code to access the innerhtml of the front page
+    const frontLogo = document.querySelector('#FrontLogo');
+
+    if (companyL) {
+        frontLogo.style.backgroundImage = `url(${companyL})`;
+    }
+    const frontcompanyName = document.querySelector('#F-companyName').innerHTML = companyN;
+    const frontPassport = document.querySelector('#F-passport');
+    if (staffP) {
+        frontPassport.style.backgroundImage = `url(${staffP})`;
+    }
+    const frontstaffName = document.querySelector('#F-staffname').innerHTML = staffName;
+    const frontStaffPosition = document.querySelector('#F-staffposition').innerHTML = staffPosition;
+    const frontStaffPhone = document.querySelector('#F-staffphone').innerHTML = staffContact;
+    const frontStaffEmail = document.querySelector('#F-staffemail').innerHMTL = staffEmail;
+    const frontStaffSignature = document.querySelector('#F-staffsignature');
+    if (staffSig) {
+        frontStaffSignature.style.backgroundImage = `url(${staffSig})`;
+    }
+    const backCertification = document.querySelector('#B-Certification').innerHTML = certText;
+    const backCompanyName = document.querySelector('#B-CompanyName').innerHTML = bcompanyName;
+    const backCompanyAddress = document.querySelector('#B-CompanyAddress').innerHTML;
+    const backCompanyPhoneNumber = document.querySelector('#B-CompanyPhoneNumber').innerHTML = companyC;
+    const bEmail = document.querySelector('#B-CompanyEmail').innerHTML = companyE;
+    const backCompanySignature = document.querySelector('#B-CompanySignature');
+    if (companySig) {
+        backCompanySignature.style.backgroundImage = `url(${companySig})`;
+    }
 
 });
+
+
+ 
+// //DOWNLOAD CODE
+
+// // Add an event listener to the download button for the front div
+// document.getElementById('downloadBtn').addEventListener('click', function() {
+//     // Call the function to convert and download the front div as an image
+//     downloadAsImage('front');
+// });
+
+// // // Add an event listener to the download button for the back div
+// // document.getElementById('downloadBackAsImage').addEventListener('click', function() {
+// //     // Call the function to convert and download the back div as an image
+// //     downloadAsImage('back');
+// // });
+
+// // Function to convert and download the div content as an image
+// function downloadAsImage(divId) {
+//     const frontPage = document.getElementById('front');
+
+//     html2canvas(frontPage).then(function(canvas) {
+//         // Convert canvas to image
+//         const image = canvas.toDataURL('image/png'); // Change format to 'image/jpeg' for JPEG format
+
+//         // Create a temporary link element
+//         const downloadLink = document.createElement('a');
+//         downloadLink.href = image;
+//         downloadLink.download = `${divId}_image.png`; // Change extension to '.jpg' for JPEG format
+
+//         // Append the link to the body
+//         document.body.appendChild(downloadLink);
+
+//         // Trigger the download
+//         downloadLink.click();
+
+//         // Remove the temporary link from the DOM
+//         document.body.removeChild(downloadLink);
+//     });
+// }
